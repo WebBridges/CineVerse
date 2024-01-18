aeac5c368f4d26bb115b6272772de76c83eba67b
Mattia Burreli <mattia.burreli@studio.unibo.it>
2024-01-18 19:30:07 +0100

    minor fix in RegistrationPage.html, added RegistrationCheck.js, Create DataBase.php for open & close connection with database and implemented functions for add a new accont and new topic.

---
+++ PHP/Utils/bootstrap.php
@@ -0,0 +1,6 @@
+<?php
+/*Raggruppiamo tutti gli elementi condivisi tra i vari php */
+
+require_once("../Database/DataBase.php");
+$dbh = new DatabaseHelper("localhost", "root", "", "cineverse", 3306);
+?>
