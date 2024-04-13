db9e891ff8ce6bc5dc31db0f2a6315929834c156
Lorenzo Bacchini <lorenzo.bacchini4@studio.unibo.it>
2024-04-13 12:29:48 +0200

    changed database instance to the online database

--- PHP/Utils/bootstrap.php
+++ PHP/Utils/bootstrap.php
@@ -7,7 +7,7 @@ require_once(__DIR__ . "/CheckInputForms.php");
 require_once(__DIR__ . "/../Database/DataBase.php");
 require_once(__DIR__ . "/../../vendor/autoload.php");
 
-$database = DataBase::getInstance("localhost", "root", "", "cineverse");
+$database = DataBase::getInstance("sql11.freesqldatabase.com", "sql11698630", "D8iesRmkrX", "sql11698630");
 $db = $database->getDatabase();
 
 function getDb() {
