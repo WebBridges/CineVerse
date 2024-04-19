d754fd7f6672bdbfdebfed1e7c082a606447b268
Lorenzo Bacchini <lorenzo.bacchini4@studio.unibo.it>
2024-04-19 23:44:46 +0200

    moved some function to utils.js and added import to them in SettingsPage.js

--- JAVASCRIPT/Profile/SettingsPage.js
+++ JAVASCRIPT/Profile/SettingsPage.js
@@ -2,6 +2,8 @@ document.addEventListener('DOMContentLoaded', async (event) => {
     await LoadUserSettingsAccount();
 });
 
+import { GetUsernameInfo } from "../Utils/utils.js";
+
 
 async function LoadUserSettingsAccount(){
 
@@ -28,45 +30,4 @@ async function LoadUserSettingsAccount(){
         document.getElementById(topicId).checked = true;
         }
         
-}
-
-
-
-async function GetUsernameInfo(){
-    let username = await GetUsername();
-    const response = await fetch('../../PHP/Profile/getUserInfo.php', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        body: `username=${username}`,
-    });
-
-    if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-    }
-
-    const contentType = response.headers.get("content-type");
-    if (contentType && contentType.indexOf("application/json") !== -1) {
-        const data = await response.json();
-        return data; 
-    } else {
-        throw new Error(`Expected JSON but received a ${contentType} and ${await response.text()}`);
-    } 
-}
-
-async function GetUsername(){
-    
-    const response = await fetch('../../PHP/Profile/getUsername.php', {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-        }
-    });
-
-    if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-    }
-    const data = await response.json();
-    return data.username;  
 }
