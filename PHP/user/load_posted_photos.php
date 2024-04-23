ae2b63be067f490b5ad3d52da5a09b2a17d79216
Lorenzo Bacchini <lorenzo.bacchini4@studio.unibo.it>
2024-04-23 23:16:35 +0200

    now photo/video post are loaded correctly if they are loaded from SearchUserPage

--- PHP/user/load_posted_photos.php
+++ PHP/user/load_posted_photos.php
@@ -1,8 +1,15 @@
 <?php
     require_once("../Utils/post.php");
     use post\PostUtility;
+
+    if(!isset($_GET['username'])) 
+    { 
+        $username = $_SESSION['username'];
+    } else {
+        $username = $_GET['username'];
+    }
     
-    $posts = PostUtility::get_posts_by_username_utente_foto_video($_SESSION['username']);
+    $posts = PostUtility::get_posts_by_username_utente_foto_video($username);
     $photos = array();
     foreach($posts as $post) {
         $photo = PostUtility::get_foto_video_post_by_IDpost($post->get_IDpost());
