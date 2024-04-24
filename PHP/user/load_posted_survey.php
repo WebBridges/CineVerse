c6327f2e4871fbc9aa24b8e5c21717da803f1a56
Lorenzo Bacchini <lorenzo.bacchini4@studio.unibo.it>
2024-04-25 01:56:19 +0200

    temp file to load survey

--- PHP/user/load_posted_survey.php
+++ PHP/user/load_posted_survey.php
@@ -1,7 +1,26 @@
 <?php
+    require_once("../Utils/post.php");
     use post\PostUtility;
+
+    if(!isset($_GET['username'])) 
+    { 
+        $username = $_SESSION['username'];
+    } else {
+        $username = $_GET['username'];
+    }
     
-    $posts = PostUtility::get_posts_by_username_utente_survey($_SESSION['username']);
+    $posts = PostUtility::get_posts_by_username_utente_survey($username);
+    $options = array();
+    foreach($posts as $post) {
+        $option = PostUtility::get_opzioni_post_by_IDpost($post->get_IDpost());
+        if($option != null) {
+            $options[$post->get_IDpost()] = $option;
+        }
+    }
+
+    $merged["posts"] = $posts;
+    $merged["options"] = $options;
+    $mergedEncoded = json_encode($merged, JSON_PRETTY_PRINT);
     header('Content-Type: application/json');
-    echo json_encode($posts, JSON_PRETTY_PRINT);
+    echo $mergedEncoded;
 ?>
