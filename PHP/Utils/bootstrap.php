87a1a5fa822bf163123863e6b1e432eaf0700fa4
Mattia Burreli <mattia.burreli@studio.unibo.it>
2024-05-26 13:41:22 +0200

    added env variables in bootstrap.php for access into database

--- PHP/Utils/bootstrap.php
+++ PHP/Utils/bootstrap.php
@@ -7,15 +7,17 @@ require_once(__DIR__ . "/CheckInputForms.php");
 require_once(__DIR__ . "/../Database/DataBase.php");
 require_once(__DIR__ . "/../../vendor/autoload.php");
 
+/*
 $host = "sql.freedb.tech";
 $user = "freedb_amministratore";
 $password = "?rTU*96%DDJMPNj";
 $dbName = "freedb_cineverse";
+*/
 
-/*$host = "localhost";
-$user = "root";
-$password = "";
-$dbName = "cineverse";*/
+$host = getenv("CV_SERVERNAME");
+$user = getenv("CV_USERNAME");
+$password = getenv("CV_PASSWORD");
+$dbName = getenv("CV_DBNAME");
 
 $database = DataBase::getInstance($host, $user, $password, $dbName);
 $db = $database->getDatabase();
