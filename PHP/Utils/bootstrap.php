cf0bcb7dfb8333c210238235e8b4352f422b989e
Mattia Burreli <mattia.burreli@studio.unibo.it>
2024-05-26 13:41:54 +0200

    minor fix

--- PHP/Utils/bootstrap.php
+++ PHP/Utils/bootstrap.php
@@ -7,13 +7,6 @@ require_once(__DIR__ . "/CheckInputForms.php");
 require_once(__DIR__ . "/../Database/DataBase.php");
 require_once(__DIR__ . "/../../vendor/autoload.php");
 
-/*
-$host = "sql.freedb.tech";
-$user = "freedb_amministratore";
-$password = "?rTU*96%DDJMPNj";
-$dbName = "freedb_cineverse";
-*/
-
 $host = getenv("CV_SERVERNAME");
 $user = getenv("CV_USERNAME");
 $password = getenv("CV_PASSWORD");
